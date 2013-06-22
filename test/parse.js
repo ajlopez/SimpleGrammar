b7135b6959ea76fdfa55f5d92e60f5e06d201fc6
ajlopez <webmaster@ajlopez.com>
2013-06-22 13:10:32 -0300

    parse a character

--- test/parse.js
+++ test/parse.js
@@ -7,3 +7,14 @@ var simpleparser = require('..'),
 assert.ok(simpleparser);
 assert.ok(simpleparser.parse);
 assert.equal(typeof simpleparser.parse, "function");
+
+var parse = simpleparser.parse;
+
+// parse a character
+
+var parser = parse('a');
+
+assert.ok(parser.parse('a'));
+assert.equal(parser.parse('a'), 'a');
+assert.equal(parser.parse('b'), null);
+
