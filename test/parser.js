4878e784587f2924f2587c8ce93f975d400f55dd
ajlopez <webmaster@ajlopez.com>
2013-06-22 18:35:45 -0300

    First rule with escape character for strings

--- test/parser.js
+++ test/parser.js
@@ -17,7 +17,7 @@ var rules = [
     get([' ','\t','\r','\n']).oneOrMore().skip(),
     get('#').upTo('\n').skip(),
     get('"').upTo('"').generate('String', function (value) { return createConstant(value.substring(1, value.length - 1)); }),
-    get("'").upTo("'").generate('String', function (value) { return createConstant(value.substring(1, value.length - 1)); }),
+    get("'").upTo("'", '\\', escape).generate('String', function (value) { return createConstant(value.substring(1, value.length - 1)); }),
     get('0-9').oneOrMore().generate('Integer', function (value) { return createConstant(parseInt(value)); }),
     get(['a-z', 'A-Z', '_'], get(['a-z', 'A-Z', '_', '0-9']).zeroOrMore()).generate('Name', function (name) { return createName(name); }),
     get(['+','-','*','/']).generate('Operator'),
@@ -27,6 +27,19 @@ var rules = [
     get('Term', 'Operator', 'Term').generate('Expression', function (values) { return createBinaryExpression(values[0].value, values[1].value, values[2].value); }),
     get('Term').generate('Expression')
 ];
+
+// escape character function
+
+function escape(ch) {
+    if (ch === 't')
+        return '\t';
+    if (ch === 'r')
+        return '\r';
+    if (ch === 'n')
+        return '\n';
+
+    return ch;
+}
 
 // factory functions
 
@@ -75,6 +88,16 @@ var result = parser.parse('String');
 assert.ok(result);
 assert.ok(result.value instanceof ConstantExpression);
 assert.equal(result.value.value, 'foo');
+
+assert.equal(parser.parse('String'), null);
+
+// Parse simple string with special characters
+
+var parser = simpleparser.createParser("'foo\\t\\n\\rbar'", rules);
+var result = parser.parse('String');
+assert.ok(result);
+assert.ok(result.value instanceof ConstantExpression);
+assert.equal(result.value.value, 'foo\t\n\rbar');
 
 assert.equal(parser.parse('String'), null);
 
