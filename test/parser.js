0498f23d1af699444c489cf019ce80c4ff6589f7
ajlopez <webmaster@ajlopez.com>
2013-06-22 17:22:03 -0300

    Parse integer

---
+++ test/parser.js
@@ -0,0 +1,32 @@
+
+var simpleparser = require('..'),
+    assert = require('assert');
+    
+// get function
+
+var get = simpleparser.get;
+
+// createParser function
+
+assert.ok(simpleparser.createParser);
+assert.equal(typeof simpleparser.createParser, "function");
+
+// rules to use
+
+var rules = [
+    get('0-9').oneOrMore().generate('Integer'),
+    get(['a-z', 'A-Z', '_'], get(['a-z', 'A-Z', '_', '0-9']).zeroOrMore()).generate('Name'),
+    get(['+','-','*','/']).generate('Operator'),
+    get(['(',')','[',']','.']).generate('Punctuation'),
+    get('Integer').generate('Term'),
+    get('Name').generate('Term'),
+    get('Term', 'Operator', 'Term').generate('Expression'),
+    get('Term').generate('Expression')
+];
+
+// Parse integer
+
+var parser = simpleparser.createParser('123', rules);
+var result = parser.parse('Integer');
+assert.ok(result);
+assert.equal(result.value, '123');
