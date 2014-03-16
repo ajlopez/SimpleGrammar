a1b603cae053e1f50c01a3d9d944fdb011e07a3e
ajlopez <webmaster@ajlopez.com>
2014-03-16 20:06:38 -0300

    Additional test, peek in a get

--- test/peek.js
+++ test/peek.js
@@ -41,3 +41,18 @@ exports['peek a word'] = function (test) {
     test.ok(result2);
     test.strictEqual(result2, 'then');
 }
+
+exports['peek in a get'] = function (test) {
+    var parser = simplegrammar.createParser('ab');
+    var rule = get('a', peek('b'));
+    
+    var result = rule.process(parser);
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 2);
+    test.strictEqual(result[0], 'a');
+    test.strictEqual(result[1], true);
+    
+    test.equal(parser.next(), 'b');
+}
