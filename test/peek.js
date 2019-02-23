dd70dbb0fd2fd4d6daff0be01ac5a6f9f7caa25a
ajlopez <webmaster@ajlopez.com>
2019-02-23 13:47:04 -0300

    Peek tests refactor

--- test/peek.js
+++ test/peek.js
@@ -1,8 +1,8 @@
 
-var simplegrammar = require('..');
+const simplegrammar = require('..');
 
-var get = simplegrammar.get;
-var peek = simplegrammar.peek;
+const get = simplegrammar.get;
+const peek = simplegrammar.peek;
 
 exports['peek function'] = function (test) {
     test.ok(simplegrammar);
@@ -11,42 +11,42 @@ exports['peek function'] = function (test) {
 }
 
 exports['peek a character'] = function (test) {
-    var parser = simplegrammar.createParser('a');
-    var rule = peek('a');
-    var rule2 = get('a');
+    const parser = simplegrammar.createParser('a');
+    const rule = peek('a');
+    const rule2 = get('a');
     
-    var result = rule.process(parser);
+    const result = rule.process(parser);
     
     test.ok(result);
     test.strictEqual(result, true);
     
-    var result2 = rule2.process(parser);
+    const result2 = rule2.process(parser);
 
     test.ok(result2);
     test.strictEqual(result2, 'a');
 }
 
 exports['peek a word'] = function (test) {
-    var parser = simplegrammar.createParser('then');
-    var rule = peek('then');
-    var rule2 = get('then');
+    const parser = simplegrammar.createParser('then');
+    const rule = peek('then');
+    const rule2 = get('then');
     
-    var result = rule.process(parser);
+    const result = rule.process(parser);
     
     test.ok(result);
     test.strictEqual(result, true);
     
-    var result2 = rule2.process(parser);
+    const result2 = rule2.process(parser);
 
     test.ok(result2);
     test.strictEqual(result2, 'then');
 }
 
 exports['peek in a get'] = function (test) {
-    var parser = simplegrammar.createParser('ab');
-    var rule = get('a', peek('b'));
+    const parser = simplegrammar.createParser('ab');
+    const rule = get('a', peek('b'));
     
-    var result = rule.process(parser);
+    const result = rule.process(parser);
     
     test.ok(result);
     test.ok(Array.isArray(result));
